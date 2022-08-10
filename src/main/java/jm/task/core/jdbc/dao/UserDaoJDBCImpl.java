319ac39d183023f267303a0455e87b8233e90bfe
AntonUdintsev <antonudincev@gmail.com>
2022-08-10 10:47:31 +0300

    v2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,73 +8,111 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private static final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS testshema1.users (" +
+        try (Connection connection = Util.getConnection()) {
+            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS testshema1.users (" +
                     "id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
                     "name VARCHAR(255), " +
                     "last_name VARCHAR(255), " +
                     "age INT)");
+            connection.createStatement().executeUpdate("COMMIT");
         } catch (SQLException e) {
             e.printStackTrace();
+            try (Connection connection = Util.getConnection()) {
+                connection.createStatement().executeUpdate("ROLLBACK");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+
         }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS testshema1.users");
+        try (Connection connection = Util.getConnection()) {
+            connection.createStatement().executeUpdate("DROP TABLE IF EXISTS testshema1.users");
+            connection.createStatement().executeUpdate("COMMIT");
         } catch (SQLException e) {
             e.printStackTrace();
+            try (Connection connection = Util.getConnection()) {
+                connection.createStatement().executeUpdate("ROLLBACK");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement pstm = connection.prepareStatement("INSERT INTO testshema1.users (name, last_name, age) VALUES (?, ?, ?)")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement pstm = connection.prepareStatement("INSERT INTO testshema1.users (name, last_name, age) VALUES (?, ?, ?)")) {
             pstm.setString(1, name);
             pstm.setString(2, lastName);
             pstm.setByte(3, age);
             pstm.executeUpdate();
             System.out.println(String.format("User с именем – %s добавлен в базу данных", name));
+            connection.createStatement().executeUpdate("COMMIT");
         } catch (SQLException e) {
             e.printStackTrace();
+            try (Connection connection = Util.getConnection()) {
+                connection.createStatement().executeUpdate("ROLLBACK");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     public void removeUserById(long id) {
-        try (PreparedStatement pstm = connection.prepareStatement("DELETE FROM testshema1.users WHERE id = ?")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement pstm = connection.prepareStatement("DELETE FROM testshema1.users WHERE id = ?")) {
             pstm.setLong(1, id);
             pstm.executeUpdate();
+            connection.createStatement().executeUpdate("COMMIT");
         } catch (SQLException e) {
             e.printStackTrace();
+            try (Connection connection = Util.getConnection()) {
+                connection.createStatement().executeUpdate("ROLLBACK");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     public List<User> getAllUsers() {
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("select * from testshema1.users");
+        try (Connection connection = Util.getConnection()) {
+            ResultSet resultSet = connection.createStatement().executeQuery("select * from testshema1.users");
             List<User> result = new ArrayList<>();
             while (resultSet.next()) {
                 User user = new User(resultSet.getString(2), resultSet.getString(3), resultSet.getByte(4));
                 user.setId(resultSet.getLong(1));
                 result.add(user);
             }
+            connection.createStatement().executeUpdate("COMMIT");
             return result;
         } catch (SQLException e) {
             e.printStackTrace();
+            try (Connection connection = Util.getConnection()) {
+                connection.createStatement().executeUpdate("ROLLBACK");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
         return null;
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("TRUNCATE TABLE testshema1.users");
+        try (Connection connection = Util.getConnection()) {
+            connection.createStatement().executeUpdate("TRUNCATE TABLE testshema1.users");
+            connection.createStatement().executeUpdate("COMMIT");
         } catch (SQLException e) {
             e.printStackTrace();
+            try (Connection connection = Util.getConnection()) {
+                connection.createStatement().executeUpdate("ROLLBACK");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
